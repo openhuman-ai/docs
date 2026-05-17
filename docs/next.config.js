c206210d63320b123f0e87e8aecca7f5a2aef5b5
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2026-05-18 01:38:18 +0700

    update docs

--- docs/next.config.js
+++ docs/next.config.js
@@ -2,7 +2,7 @@ const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.js",
   staticImage: true,
-  latex:true,
+  latex: true,
   flexsearch: {
     codeblocks: false,
   },
